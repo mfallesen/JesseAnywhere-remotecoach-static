bccaaf1ae0f286e9d85f6b0b886e6d585bbf6254
Mike Fallesen <mfallesen2010@gmail.com>
2022-02-15 17:11:08 -0800

    feature(Form Data): grab user entered info from the form and successfully send it to server. Must implement validation prior to production

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,22 @@
 
 // =======THIS MUST BE CHANGE DON DEPLOYED APPLICATION =======
 const mailApi = 'http://localhost:3001/api/mailto'
-//  Mock Call 
-let mockCall = {
-    "sender": "John",
-	"senderEmail": "john@john.com",
-	"subject": "Mail Testing",
-	"planInterest": "Kinda Fit",
-	"message": "This is a test message to see that everything is kosher"
-}
-
 
 let sendBtn = document.getElementById('contactform-submit')
 
-sendBtn.addEventListener('click', function(event) {
+sendBtn.addEventListener('click', function (event) {
     event.preventDefault();
-    mailSend(mockCall);
-}) 
+console.log(document.getElementById('contactform-name').value);
+
+    let submissionData = {
+        "sender": document.getElementById('contactform-name').value,
+        "senderEmail": document.getElementById('contactform-email').value,
+        "subject": document.getElementById('contactform-subject').value,
+        "planInterest": document.getElementById('contactform-service').value,
+        "message": document.getElementById('contactform-message').value
+    }
+    mailSend(submissionData);
+})
 
 
 
@@ -27,27 +27,19 @@ function mailSend(dataSent) {
     // let formData = new FormData(dataSent)
 
     console.log(dataSent);
-    
+
     console.log("Hello there");
     // fetch request to mail API
     fetch(mailApi, {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
-        // mode: "no-cors",
-        // body: JSON.stringify ({
-        //     "sender": "John",
-        //     "senderEmail": "john@john.com",
-        //     "subject": "Mail Testing",
-        //     "planInterest": "Kinda Fit",
-        //     "message": "This is a test message to see that everything is kosher"
-        // })
-        body: JSON.stringify(mockCall)
+        body: JSON.stringify(dataSent)
     })
-    .then(function(response) {
-        if (!response) {
-            throw new Error("Ooops Something went Wrong Please try again later")
-        }
-        console.log("=== response ===", response.status);
-    })
+        .then(function (response) {
+            if (!response) {
+                throw new Error("Ooops Something went Wrong Please try again later")
+            }
+            console.log("=== response ===", response.status);
+        })
 
 }
